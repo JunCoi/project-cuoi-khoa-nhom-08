d4370b05e04ed22db4b87fa8e6de28d4fd190a07
JunCoi <juncoi94@gmail.com>
2021-06-28 17:10:32 +0700

    add close fragment

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Header from "../components/Header";
-import MovieSlider from "../components/MovieSlider";
-import BookingTool from "../components/BookingTool";
-import MoviesList from "../components/MoviesList";
-import BlockApp from "../components/BlockApp";
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import MovieSlider from '../components/MovieSlider';
+import BookingTool from '../components/BookingTool';
+import MoviesList from '../components/MoviesList';
+import BlockApp from '../components/BlockApp';
 
-import { useDispatch } from "react-redux";
-import { getMovieListAction } from "../store/actions/movieAction";
-import Cinema from "../components/Cinema";
-import { getCinemaListAction } from "../store/actions/cinemaAction";
-import News from "../components/News";
-import Footer from "../components/Footer";
+import { useDispatch } from 'react-redux';
+import { getMovieListAction } from '../store/actions/movieAction';
+import Cinema from '../components/Cinema';
+import { getCinemaListAction } from '../store/actions/cinemaAction';
+import News from '../components/News';
+import Footer from '../components/Footer';
 
 export default function HomePage() {
   const [dangChieu, setDangChieu] = useState('GP01');
@@ -36,5 +36,6 @@ export default function HomePage() {
       <BlockApp />
       <Footer />
       {/* <div style={{ height: "100px" }}></div> */}
+    </>
   );
 }
