84bc46bffede8d2eb05fa4ed4a3ee2b338a41424
Mạnh Hùng Huỳnh <huynhmanhhung2000@gmail.com>
2021-07-03 18:26:54 +0700

    update

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { authReducer } from './authReducer';
 import { cinemaReducer } from './cinemaReducer';
 import { movieReducer } from './movieReducer';
 import { popupReducer } from './popupReducer';
+import { profileReducer } from './profileReducer';
 
 export const rootReducer = combineReducers({
   popup: popupReducer,
   movieList: movieReducer,
   cinema: cinemaReducer,
   auth: authReducer,
+  profile: profileReducer,
 });
 
 
